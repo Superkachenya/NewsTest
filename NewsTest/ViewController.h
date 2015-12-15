9bc50911a1667946abaad5a7baa87c413ff73704
Superkachenya <superkachenya@gmail.com>
2015-12-15 19:47:13 +0200

    create new repository

---
+++ NewsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsTest
+//
+//  Created by Voitenko Daniil on 15.12.15.
+//  Copyright © 2015 Voitenko Daniil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsTest
+//
+//  Created by Voitenko Daniil on 15.12.15.
+//  Copyright © 2015 Voitenko Daniil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
